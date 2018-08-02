798844187ed925c34ad213569dbfbc67d4fd9bb6
Louis COUTIN <louiscoutin@gmail.com>
2018-08-02 15:03:32 +0200

    changement

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         </div>
       </nav>
     <section class="container ma-section">
-      <h1 class="text-center mt-4 display-4">Bienvenue dans ma boutique</h1>
+      <h1 class="text-center mt-4 display-4">Bienvenue dans ma super boutique !!!!!!!!</h1>
 
     <form>
       <div class="form-group">
